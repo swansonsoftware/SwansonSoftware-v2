f5be25f01c135972ba528169c61de70657fab006
swansonsoftware <greg@swansonsoftware.com>
2025-01-27 10:28:58 -0800

    Add home page banner image

--- app/components/Home.js
+++ app/components/Home.js
@@ -38,6 +38,11 @@ function Home() {
     <>
       <div className="wrapper wrapper--banner">
         <meta name="description" content="Swanson Software, disseminating knowledge in the craft of software construction" />
+        <picture className="banner__image">
+          <source srcSet="../assets/images/0a-hero.webp" media="(min-width: 1380px)" width="1920" height="950"></source>
+          <source srcSet="../assets/images/0a-hero--medium.webp" media="(min-width: 640px)" width="1380" height="950"></source>
+          <img src="../assets/images/0a-hero--small.webp" width="640" height="660" alt="Kauai Beach moonrise"></img>
+        </picture>
         <div className="banner__darkBackground banner__text-content">
           <h1 className="banner__title">Welcome to Swanson Software</h1>
           <p className="banner__subtitle">Disseminating knowledge in the craft of software construction</p>
