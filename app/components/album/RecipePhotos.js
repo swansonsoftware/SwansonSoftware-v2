bd6bde5299b40e3052aa45e4a20da1ad3bf8482e
swansonsoftware <greg@swansonsoftware.com>
2025-02-24 08:17:24 -0800

    Remove unused useEffect

--- app/components/album/RecipePhotos.js
+++ app/components/album/RecipePhotos.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import React, { useRef } from "react"
 import PhotoBlock from "./PhotoBlock"
 import PhotoCaptionBox from "./PhotoCaptionBox"
 import PhotoOverlayEvents from "./PhotoOverlayEvents"
