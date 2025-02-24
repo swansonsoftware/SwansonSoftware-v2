7a1e2fabb7c23c4cafd068056a12e8c398092995
swansonsoftware <greg@swansonsoftware.com>
2025-02-24 08:29:23 -0800

    Add Contact Us link; add tabindex = 0 so tabbing works on Apple machines and iPad

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -11,8 +11,13 @@ function Footer(props) {
       <div className="wrapper--site-footer">
         <span className="footer__copyright">Copyright &copy; &nbsp;{currentDate.getFullYear()}. All rights reserved.</span>
         <span className="footer__about-us-link">
-          <Link to="/about-us" id="about-us-link">
+          <Link to="/about-us" id="about-us-link" tabIndex={0}>
             About SwansonSoftware.com
+          </Link>
+        </span>
+        <span className="footer__contact-us-link">
+          <Link to="/contact" id="contact-us-link">
+            Contact us
           </Link>
         </span>
       </div>
