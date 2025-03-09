60fb29c8e49d47124027bafc700d332909b43ca0
swansonsoftware <greg@swansonsoftware.com>
2025-03-08 17:01:28 -0800

    Update wording for first blog excerpt and remove second blog excerpt

--- app/components/blog/_2025.js
+++ app/components/blog/_2025.js
@@ -19,30 +19,19 @@ function Blogs2025() {
       <div className="wrapper wrapper__article">
         <h1 className="headline__h1-cg">Blog List 2025</h1>
         <div className="row row--gutters">
-          <div className="row__col-span-4 row__margin-bottom-0">
-            <h2 className="headline__h2">Resolution Switching Images</h2>
+          <div className="row__colspan-4">
+            <h2 className="headline__h2">Responsive Images</h2>
           </div>
-          <div className="row__col-span-8 row__margin-bottom-0">
+          <div className="row__colspan-8">
             <h3 className="headline__h3">Choosing Resolution Switching Breakpoints</h3>
-            <p>The question of how to choose breakpoints when resolution switching images on a webpage comes up a lot, and frankly the solutions recommended by so many experts fall far short. </p>
+            <p>The question of how many breakpoints are needed and how to choose them comes up a lot when doing resolution switching of images for a responsive design. In this post I describe a technique based on an analysis of device sizes to answer the question.</p>
             <p>
-              <Link to="/blog/2025/01/resolution-switching-images">Resolution Switching...</Link>
+              <Link to="/blog/2025/01/resolution-switching-images">Choosing Resolution Switching Breakpoints...</Link>
             </p>
           </div>
 
-          <div className="row__col-span-4 row__margin-bottom-0"></div>
-          <div className="row__col-span-8 row__margin-bottom-0">&nbsp;</div>
-
-          <div className="row__col-span-4 row__margin-bottom-0">
-            <h2 className="headline__h2">Time vs. Quality</h2>
-          </div>
-          <div className="row__col-span-8 row__margin-bottom-0">
-            <h3 className="headline__h3">Time for Commenting Code</h3>
-            <p>Sometimes code is written in a way that makes it look inefficient, wrong, or just plain confusing. There is risk that sometime in the future a bug will be created by some unknowing developer who attempts to "fix" it. This is where a well-written comment is needed, and it takes time, time that is difficult to anticipate and quantify in the sprint timeline.</p>
-            <p>
-              <Link to="/blog/2025/02/time-vs-quality">Time vs. Quality...</Link>
-            </p>
-          </div>
+          <div className="row__colspan-4"></div>
+          <div className="row__colspan-8">&nbsp;</div>
         </div>
       </div>
     </Page>
