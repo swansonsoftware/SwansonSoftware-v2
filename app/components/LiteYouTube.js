88cf0850979a5c896dead7ca17347b4044ed3d7d
swansonsoftware <greg@swansonsoftware.com>
2025-03-11 10:21:46 -0700

    remove unused include

--- app/components/LiteYouTube.js
+++ app/components/LiteYouTube.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext } from "react"
 import StateContext from "../StateContext"
 import LiteYTEmbed from "../assets/scripts/modules/lite-yt-embed"
 
