e59e5d52ff13690513a3c8e68b98380063aa03d7
swansonsoftware <greg@swansonsoftware.com>
2025-04-05 15:54:23 -0700

    Add tabindex attribute to buttons so tabbing works in Safari

--- app/components/album/SlideShowVcrButtons.js
+++ app/components/album/SlideShowVcrButtons.js
@@ -3,7 +3,7 @@ import React from "react"
 function SlideShowVcrButtons() {
   return (
     <div className="slideshow__vcr-container">
-      <button id="toggleplay" className="slideshow__vcr-container__vcr-button">
+      <button id="toggleplay" tabIndex={"0"} className="slideshow__vcr-container__vcr-button">
         <span className="accessibility--hidden">Select to alternately pause and start the slideshow</span>
         <svg id="pause" className="slideshow__vcr-container--svg" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
           <path className="slideshow__vcr-container--svg--path" d="M13 1.625c6.272 0 11.375 5.103 11.375 11.375S19.272 24.375 13 24.375 1.625 19.272 1.625 13 6.728 1.625 13 1.625M13 0C5.82 0 0 5.82 0 13s5.82 13 13 13 13-5.82 13-13S20.18 0 13 0z" />
@@ -14,20 +14,20 @@ function SlideShowVcrButtons() {
           <path className="slideshow__vcr-container--svg--polygon" d="m17.708 13-6.616-4.8v9.6z" />
         </svg>
       </button>
-      <button id="prev" className="slideshow__vcr-container__vcr-button">
+      <button id="prev" tabIndex={"0"} className="slideshow__vcr-container__vcr-button">
         <span className="accessibility--hidden">Select for previous slide</span>
         <svg id="prevslide" className="slideshow__vcr-container--svg-dbl-stroke" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
           <path className="slideshow__vcr-container--svg-dbl-stroke--path" d="m17.793 3.481-9.586 9.527M17.78 22.519l-9.573-9.511" />
         </svg>
       </button>
       <span>&nbsp;&nbsp;&nbsp;</span>
-      <button id="next" className="slideshow__vcr-container__vcr-button">
+      <button id="next" tabIndex={"0"} className="slideshow__vcr-container__vcr-button">
         <span className="accessibility--hidden">Select for next slide</span>
         <svg id="nextslide" className="slideshow__vcr-container--svg-dbl-stroke" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
           <path className="slideshow__vcr-container--svg-dbl-stroke--path" d="m8.207 3.481 9.586 9.527M8.22 22.519l9.573-9.511" />
         </svg>
       </button>
-      <button id="fullscreen" className="slideshow__vcr-container__vcr-button">
+      <button id="fullscreen" tabIndex={"0"} className="slideshow__vcr-container__vcr-button">
         <span className="accessibility--hidden">Select to toggle full screen</span>
         <svg id="showFullscreen" className="slideshow__vcr-container--svg-dbl-stroke" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg">
           <path className="slideshow__vcr-container--svg-dbl-stroke--path" d="M19.494 25h5.505M19.494 1H25M1 1h5.501M1 25v-5.503M1 25h5.507M1 6.506V1M25 6.506V1M24.999 25v-5.503" />
